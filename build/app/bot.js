96e67103f8ab9ada6f77061b1b626983e9ab0a06
Mateo Deget <degetmateo@gmail.com>
2023-06-26 13:46:00 -0300

    error fix? respuestas eliminadas

--- build/app/bot.js
+++ build/app/bot.js
@@ -135,52 +135,6 @@ class BOT extends discord_js_1.Client {
                 }
             }
         });
-        this.on("messageCreate", async (message) => {
-            if (!message)
-                return;
-            if (!message.content)
-                return;
-            if (!message.guild)
-                return;
-            if (message.author.bot)
-                return;
-            const mContent = message.content.toLowerCase()
-                .split("á").join("a")
-                .split("é").join("e")
-                .split("í").join("i")
-                .split("ó").join("o")
-                .split("ú").join("u")
-                .split("?").join("")
-                .split("¿").join("");
-            if (!mContent)
-                return;
-            const CONDICION_RESPUESTA_QUE = mContent.endsWith(" que") || mContent == "que";
-            const CONDICION_RESPUESTA_TRECE = mContent.endsWith(" 13") || mContent == "13" || mContent.endsWith("trece");
-            const CONDICION_RESPUESTA_DOCE = mContent.endsWith(" 12") || mContent == "12" || mContent.endsWith("doce");
-            const CONDICION_RESPUESTA_CINCO = mContent.endsWith(" 5") || mContent == "5" || mContent.endsWith("cinco");
-            const CONDICION_RESPUESTA_CONTEXTO = mContent.endsWith(" contexto") || mContent == "contexto";
-            if (message.content === "Hola") {
-                message.reply(`${message.client.emojis.cache.find((e => e.name === "pala"))}`);
-            }
-            ;
-            if (CONDICION_RESPUESTA_QUE) {
-                return message.reply("so");
-            }
-            if (CONDICION_RESPUESTA_DOCE) {
-                message.reply("las de doce son goood");
-            }
-            ;
-            if (CONDICION_RESPUESTA_CINCO) {
-                message.reply("por el culo te la hinco");
-            }
-            if (CONDICION_RESPUESTA_TRECE) {
-                message.reply("¿Dijiste 13? Aquí tiene pa' que me la bese, entre más me la beses más me crece, busca un cura pa' que me la rese, y trae un martillo pa' que me la endereces, por el chiquito se te aparece toas las veces y cuando te estreses aquí te tengo éste pa' que te desestreses, con este tallo el jopo se te esflorece, se cumple el ciclo hasta que anochece, to' los días y toas las veces, de tanto entablar la raja del jopo se te desaparece, porque este sable no se compadece, si pides ñapa se te ofrece, y si repites se te agradece, no te hace rico pero tampoco te empobrece, no te hace inteligente pero tampoco te embrutece, y no paro aquí compa que éste nuevamente se endurece, hasta que amanece, cambie esa cara que parece que se entristece, si te haces viejo éste te rejuvenece, no te hago bulla porque depronto te ensordece, y ese cuadro no te favorece, pero tranquilo que éste te abastece, porque allá abajo se te humedece, viendo como el que me cuelga resplandece, si a ti te da miedo a mí me enorgullece, y así toas las vece ¿que te parece?, y tranquilo mijo que aquí éste reaparece, no haga fuerza porque éste se sobrecrece, una fresadora te traigo pa' que me la freses, así se fortalece y de nuevo la historia se establece, que no se te nuble la vista porque éste te la aclarece, y sino le entendiste nuevamente la explicación se te ofrece, pa' que por el chiquito éste de nuevo te empiece... Aquí tienes para que me la beses, entre más me la beses más me crece, busca un cura para que me la rece, un martillo para que me la endereces, un chef para que me la aderece, 8000 mondas por el culo se te aparecen, si me la sobas haces que se me espese, si quieres la escaneas y te la llevas para que en tu hoja de vida la anexes, me culeo a tu maldita madre y qué te parece le meti la monda a tú mamá hace 9 meses y después la puse a escuchar René de Calle 13 Te la meto por debajo del agua como los peces, y aquella flor de monda que en tu culo crece, reposa sobre tus nalgas a veces y una vez más...");
-            }
-            ;
-            if (CONDICION_RESPUESTA_CONTEXTO) {
-                message.reply("Espera dijiste contexto? Te la tragas sin pretexto, así no estés dispuesto, pero tal vez alguna vez te lo has propuesto, y te seré honesto te haré el favor y te lo presto, tan fuerte que tal vez me den arresto, ya no aguantas ni el sexto, así que lo dejamos pospuesto, pero te falta afecto y te lo dejo otra vez puesto, te aplastó en la pared como insecto tan duro que sale polvo de asbesto, llamo al arquitecto Alberto y al modesto Ernesto, y terminas más abierto que portón de asentamiento, ya no tenes más almacenamiento así que necesitas asesoramiento y a tu madre llamamos para darle su afecto así hasta el agotamiento y al siguiente día repetimos y así terminó y te la meto sin pretexto, así no estés dispuesto, pero tal vez alguna vez te lo has propuesto, y te seré honesto te haré el favor y te lo presto, tan fuerte que tal vez me den arresto, ya no aguantas ni el sexto, así que lo dejamos pospuesto, pero te falta afecto y te lo dejo otra vez puesto, te aplastó en la pared como insecto tan duro que sale polvo de asbesto, llamo al arquitecto Alberto y al modesto Ernesto, y terminas más abierto que portón de asentamiento, ya no tenes más almacenamiento así que necesitas asesoramiento y a tu madre llamamos para darle su afecto así hasta el agotamiento y al siguiente día repetimos pero ya estás descompuesto así que para mí continuar sería incorrecto y me voy sin mostrar algún gesto, dispuesto a seguir apenas y ya estés compuesto voy y te doy el impuesto pero no sin antes avisarte que este es el contexto 👍.");
-            }
-        });
         this.login(token);
     }
 }
