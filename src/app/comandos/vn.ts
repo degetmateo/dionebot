21b48d0004f8d8f5f80aea8c2ee44722488aad2f
Mateo Deget <degetmateo@gmail.com>
2023-06-27 22:36:15 -0300

    comando VN

--- src/app/comandos/vn.ts
+++ src/app/comandos/vn.ts
@@ -1,6 +1,8 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder, ColorResolvable } from "discord.js";
 import fetch from "node-fetch";
 import BOT from "../bot";
+
+const translate = require("translate");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -19,11 +21,13 @@ module.exports = {
     execute: async (interaccion: ChatInputCommandInteraction) => {
         const bot = interaccion.client as BOT;
 
+        await interaccion.deferReply();
+
         const criterio = interaccion.options.getString('nombre-o-id');
 
         if (!criterio) throw new Error('No se ha encontrado el criterio de busqueda.');
 
-        const traducir = interaccion.options.getBoolean('traducir') || false;
+        const traducirSinopsis = interaccion.options.getBoolean('traducir') || false;
         
         let tipoCriterio: 'search' | 'id';
 
@@ -39,19 +43,59 @@ module.exports = {
                 'Accept': 'application/json',
             },
             
-            data: {
+            body: JSON.stringify({ 
                 "filters": [tipoCriterio, "=", criterio],
-                "fields": "title, image.url"
-            }
+                "fields": "title, image.url, devstatus, description"
+            })
         };
 
         const peticionVN = await fetch(apiURL, peticion);
         const respuesta = await peticionVN.json();
 
-        console.log(respuesta);
+        const resultado = respuesta.results[0]
+
+        // console.log(resultado);
+
+        const id = resultado.id;
+        const vnURL = `https://vndb.org/v${id}`;
+
+        const estados = ['FINALIZADA', 'EN DESARROLLO', 'CANCELADA'];
+
+        const embed = new EmbedBuilder()
+            .setTitle(resultado.title)
+            .setURL(vnURL)
+            .setDescription(traducirSinopsis ?  await traducir(resultado.description) : resultado.description)
+            .setThumbnail(resultado.image.url)
+            .setColor(getColorEstado(estados[resultado.devstatus]));
+    
+
+        const informacionCampos1 = `
+            ‣ **Estado**: ${estados[resultado.devstatus]}
+        `;
+
+        embed.addFields({ name: "▽", value: informacionCampos1, inline: true })
+    
+        interaccion.editReply({ embeds: [embed] });
     }
 }
 
 const esNumero = (args: string) => {
     return !(isNaN(+args) || isNaN(parseFloat(args)));
-} 
+}
+
+const traducir = async (args: string) => {
+    return await translate(args, "es");
+}
+
+const getColorEstado = (estado: string): ColorResolvable => {
+    let hex = "";
+
+    switch (estado) {
+        case "FINALIZADA": hex = "00D907"; break;
+        case "EN DESARROLLO": hex = "FFF700"; break;
+        case "CANCELADA": hex = "FF0000"; break;
+        default: hex = "000000"; break;
+    }
+
+    return ("#" + hex) as ColorResolvable;
+}
