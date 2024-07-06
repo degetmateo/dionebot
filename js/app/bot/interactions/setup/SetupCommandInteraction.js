4ea745d0f69ba7ed87137a8a08376519fe1e2a42
Mateo Deget <degetmateo@gmail.com>
2024-07-06 19:08:15 -0300

    SetupCommandInteraction

--- js/app/bot/interactions/setup/SetupCommandInteraction.js
+++ js/app/bot/interactions/setup/SetupCommandInteraction.js
@@ -3,10 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = require("discord.js");
 const CommandInteraction_1 = __importDefault(require("../CommandInteraction"));
 const GenericException_1 = __importDefault(require("../../../errors/GenericException"));
-const NoResultsException_1 = __importDefault(require("../../../errors/NoResultsException"));
-const Helpers_1 = __importDefault(require("../../Helpers"));
 const AnilistAPI_1 = __importDefault(require("../../apis/anilist/AnilistAPI"));
 const Embed_1 = __importDefault(require("../../embeds/Embed"));
 const postgres_1 = __importDefault(require("../../../database/postgres"));
@@ -16,67 +15,177 @@ class SetupCommandInteraction extends CommandInteraction_1.default {
         this.interaction = interaction;
     }
     async execute() {
-        const query = this.interaction.options.getString('nombre-o-id');
-        const serverId = this.interaction.guild.id;
-        const userId = this.interaction.user.id;
-        let queryUserCount = await postgres_1.default.query() `
-            SELECT user_count FROM 
-                discord_server
-            WHERE
-                id_server = ${serverId};
-        `;
-        const userCount = queryUserCount[0].user_count;
-        if (userCount >= SetupCommandInteraction.MAX_USERS) {
-            throw new GenericException_1.default('Se ha alcanzado la cantidad máxima de usuarios registrados en este servidor.');
-        }
-        let querMembership = await postgres_1.default.query() `
+        const queryMembership = await postgres_1.default.query() `
             SELECT * FROM
                 membership
             WHERE
-                id_user = ${userId} and
-                id_server = ${serverId};
+                id_user = ${this.interaction.user.id} and
+                id_server = ${this.interaction.guild.id};
         `;
-        if (querMembership[0]) {
-            throw new GenericException_1.default('Ya te encuentras registrado.');
+        if (queryMembership[0]) {
+            throw new GenericException_1.default('Ya te encuentras registrado. Si deseas actualizar tu informacion de anilist, primero utiliza \`/unsetup\`.');
         }
-        let anilistUser;
+        const queryServer = await postgres_1.default.query() `
+            SELECT * FROM
+                discord_server
+            WHERE
+                id_server = ${this.interaction.guild.id};
+        `;
+        if (queryServer[0].user_count >= SetupCommandInteraction.MAX_USERS) {
+            throw new GenericException_1.default('Se ha alcanzado el número máximo de usuarios registrados en este servidor.');
+        }
         try {
-            anilistUser = Helpers_1.default.isNumber(query) ?
-                await AnilistAPI_1.default.fetchUserById(parseInt(query)) : await AnilistAPI_1.default.fetchUserByName(query);
-        }
-        catch (error) {
-            if (error instanceof NoResultsException_1.default) {
-                throw new NoResultsException_1.default('No se ha encontrado el usuario proporcionado en anilist.');
-            }
-        }
-        await postgres_1.default.query().begin(async (sql) => {
-            await sql ` 
-                SELECT insert_user (
-                    ${userId},
-                    ${anilistUser.getId()}
-                );
-            `;
-            await sql `
-                SELECT insert_membership (
-                    ${userId},
-                    ${serverId}
-                );
-            `;
-        });
-        const embed = Embed_1.default.Crear()
-            .establecerColor(Embed_1.default.COLOR_VERDE)
-            .establecerDescripcion('Te has registrado con éxito.')
-            .obtenerDatos();
-        try {
-            await this.interaction.reply({
-                embeds: [embed],
-                ephemeral: true
+            const res = await this.interaction.reply({
+                embeds: [this.createInstructionsEmbed()],
+                components: [this.createInstructionsRow()],
+                ephemeral: true,
             });
+            await this.createCollector(res);
         }
         catch (error) {
             throw error;
         }
     }
+    createInstructionsEmbed() {
+        return new discord_js_1.EmbedBuilder()
+            .setColor(0xff8c00)
+            .setTitle("Instrucciones")
+            .setDescription(EMBED_INSTRUCTIONS_DESCRIPTION)
+            .setFooter({ text: "No compartas los códigos con nadie." });
+    }
+    createInstructionsRow() {
+        const buttonURL = new discord_js_1.ButtonBuilder({
+            style: discord_js_1.ButtonStyle.Link,
+            url: process.env.ANILIST_AUTH_URL,
+            label: 'Autorizar',
+        });
+        const buttonCode = new discord_js_1.ButtonBuilder({
+            style: discord_js_1.ButtonStyle.Primary,
+            custom_id: 'buttonCode',
+            label: 'Ingresar código',
+        });
+        return new discord_js_1.ActionRowBuilder().addComponents(buttonURL, buttonCode);
+    }
+    async createCollector(res) {
+        const collector = res.createMessageComponentCollector({
+            time: CommandInteraction_1.default.TIEMPO_ESPERA_INTERACCION
+        });
+        collector.on('collect', async (button) => {
+            if (button.customId === 'buttonCode') {
+                let modal;
+                try {
+                    await button.showModal(this.createModal());
+                    modal = await button.awaitModalSubmit({ time: CommandInteraction_1.default.TIEMPO_ESPERA_INTERACCION });
+                    await modal.deferReply({ ephemeral: true });
+                    await this.validateToken(modal);
+                }
+                catch (error) {
+                    console.error(error);
+                    if (modal) {
+                        modal.editReply({
+                            embeds: [
+                                new discord_js_1.EmbedBuilder()
+                                    .setColor(Embed_1.default.COLOR_RED)
+                                    .setDescription('Ha ocurrido un error. Intentalo de nuevo mas tarde.')
+                            ]
+                        });
+                    }
+                }
+            }
+        });
+    }
+    createModal() {
+        const codeInput = new discord_js_1.TextInputBuilder()
+            .setCustomId('inputCode')
+            .setLabel('Tu código')
+            .setStyle(discord_js_1.TextInputStyle.Paragraph);
+        const row = new discord_js_1.ActionRowBuilder().addComponents(codeInput);
+        const modal = new discord_js_1.ModalBuilder()
+            .setCustomId('modalCode')
+            .setTitle('Pega tu código aquí');
+        return modal.addComponents(row);
+    }
+    async validateToken(modal) {
+        const token = modal.fields.getTextInputValue('inputCode');
+        const query = `
+            query {
+                Viewer {
+                    id
+                    name
+                    siteUrl
+                }
+            }
+        `;
+        const results = await AnilistAPI_1.default.authorizedFetch(token, query);
+        if (results.errors || !results.data) {
+            await modal.editReply({
+                embeds: [
+                    new discord_js_1.EmbedBuilder()
+                        .setColor(Embed_1.default.COLOR_RED)
+                        .setDescription('Token ingresado invalido. Verifica que lo hayas copiado bien.')
+                ]
+            });
+            console.error(results.errors);
+            return;
+        }
+        try {
+            await this.saveUser(results.data.Viewer, token);
+            const desc = `
+Autentificación completada correctamente como [${results.data.Viewer.name}](${results.data.Viewer.siteUrl})
+▸ Puedes utilizar \`/usuario\` para comprobar tu perfil.                
+▸ Si lo deseas o cambias de cuenta de anilist, deberás utilizar \`/unsetup\` para desvincular tu cuenta y luego realizar este proceso nuevamente.
+▸ Para conocer otros comandos, utiliza \`/help\`.
+`;
+            const embedAuthorization = new discord_js_1.EmbedBuilder()
+                .setDescription(desc)
+                .setColor(Embed_1.default.COLOR_GREEN);
+            await modal.editReply({
+                embeds: [embedAuthorization]
+            });
+        }
+        catch (error) {
+            console.error(error);
+            await modal.editReply({
+                embeds: [
+                    new discord_js_1.EmbedBuilder()
+                        .setColor(Embed_1.default.COLOR_RED)
+                        .setDescription('Ha ocurrido al intentar guardar el usuario. Intentalo de nuevo mas tarde.')
+                ]
+            });
+            return;
+        }
+    }
+    async saveUser(viewer, token) {
+        await postgres_1.default.query().begin(async (sql) => {
+            await sql `
+                DELETE FROM 
+                    discord_user
+                WHERE
+                    id_user != ${this.interaction.user.id} and
+                    id_anilist = ${viewer.id};
+            `;
+            await sql ` 
+                SELECT insert_user (
+                    ${this.interaction.user.id},
+                    ${viewer.id},
+                    ${token}
+                );
+            `;
+            await sql `
+                SELECT insert_membership (
+                    ${this.interaction.user.id},
+                    ${this.interaction.guild.id}
+                );
+            `;
+        });
+    }
 }
-SetupCommandInteraction.MAX_USERS = 15;
+SetupCommandInteraction.MAX_USERS = 20;
 exports.default = SetupCommandInteraction;
+const EMBED_INSTRUCTIONS_DESCRIPTION = `
+▸ Haz click en el botón **Autorizar**.
+▸ Una vez dentro de la página web, debes presionar en **Authorize**.
+▸ Se generará tu código de autentificación, debes copiarlo.
+▸ Cierra la página web y vuelve aquí, haz click en el botón **Ingresar código**.
+▸ Pega el código en el formulario y envíalo.
+`;
--- src/app/bot/interactions/setup/SetupCommandInteraction.ts
+++ src/app/bot/interactions/setup/SetupCommandInteraction.ts
@@ -1,15 +1,12 @@
-import { ChatInputCommandInteraction, CacheType } from "discord.js";
+import { ChatInputCommandInteraction, CacheType, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, InteractionResponse, TextInputBuilder, ModalBuilder, TextInputStyle, ModalSubmitInteraction } from "discord.js";
 import CommandInteraction from "../CommandInteraction";
 import GenericException from "../../../errors/GenericException";
-import NoResultsException from "../../../errors/NoResultsException";
-import Helpers from "../../Helpers";
 import AnilistAPI from "../../apis/anilist/AnilistAPI";
-import AnilistUser from "../../apis/anilist/modelos/AnilistUser";
 import Embed from "../../embeds/Embed";
 import Postgres from "../../../database/postgres";
 
 export default class SetupCommandInteraction extends CommandInteraction {
-    private static readonly MAX_USERS = 15;
+    private static readonly MAX_USERS = 20;
     protected interaction: ChatInputCommandInteraction<CacheType>;
     
     constructor (interaction: ChatInputCommandInteraction<CacheType>) {
@@ -17,75 +14,208 @@ export default class SetupCommandInteraction extends CommandInteraction {
         this.interaction = interaction;
     }
 
-    public async execute(): Promise<void> {
-        const query = this.interaction.options.getString('nombre-o-id');
-
-        const serverId = this.interaction.guild.id;
-        const userId = this.interaction.user.id;
-
-        let queryUserCount: Array<{ user_count: number }> = await Postgres.query() `
-            SELECT user_count FROM 
-                discord_server
-            WHERE
-                id_server = ${serverId};
-        `;
-
-        const userCount = queryUserCount[0].user_count;
-        
-        if (userCount >= SetupCommandInteraction.MAX_USERS) {
-            throw new GenericException('Se ha alcanzado la cantidad máxima de usuarios registrados en este servidor.');
-        }
-
-        let querMembership: any = await Postgres.query() `
+    public async execute (): Promise<void> {
+        const queryMembership = await Postgres.query() `
             SELECT * FROM
                 membership
             WHERE
-                id_user = ${userId} and
-                id_server = ${serverId};
+                id_user = ${this.interaction.user.id} and
+                id_server = ${this.interaction.guild.id};
         `;
 
-        if (querMembership[0]) {
-            throw new GenericException('Ya te encuentras registrado.');
+        if (queryMembership[0]) {
+            throw new GenericException ('Ya te encuentras registrado. Si deseas actualizar tu informacion de anilist, primero utiliza \`/unsetup\`.');
         }
 
-        let anilistUser: AnilistUser;
+        const queryServer = await Postgres.query() `
+            SELECT * FROM
+                discord_server
+            WHERE
+                id_server = ${this.interaction.guild.id};
+        `;
+
+        if (queryServer[0].user_count >= SetupCommandInteraction.MAX_USERS) {
+            throw new GenericException ('Se ha alcanzado el número máximo de usuarios registrados en este servidor.');
+        }
+
         try {
-            anilistUser = Helpers.isNumber(query) ?
-                await AnilistAPI.fetchUserById(parseInt(query)) : await AnilistAPI.fetchUserByName(query);
+            const res = await this.interaction.reply ({
+                embeds: [this.createInstructionsEmbed()],
+                components: [this.createInstructionsRow()],
+                ephemeral: true,
+            })
+    
+            await this.createCollector(res);            
         } catch (error) {
-            if (error instanceof NoResultsException) {
-                throw new NoResultsException('No se ha encontrado el usuario proporcionado en anilist.');
+            throw error;   
+        }
+    }
+
+    private createInstructionsEmbed (): EmbedBuilder {
+        return new EmbedBuilder()            
+            .setColor(0xff8c00)
+            .setTitle("Instrucciones")
+            .setDescription(EMBED_INSTRUCTIONS_DESCRIPTION)
+            .setFooter({ text: "No compartas los códigos con nadie." });
+    }
+
+    private createInstructionsRow (): ActionRowBuilder<ButtonBuilder> {
+        const buttonURL = new ButtonBuilder({
+            style: ButtonStyle.Link,
+            url: process.env.ANILIST_AUTH_URL,
+            label: 'Autorizar',
+        })
+
+        const buttonCode = new ButtonBuilder({
+            style: ButtonStyle.Primary,
+            custom_id: 'buttonCode',
+            label: 'Ingresar código',
+        })
+
+        return new ActionRowBuilder<ButtonBuilder>().addComponents(buttonURL, buttonCode);
+    }
+
+    private async createCollector (res: InteractionResponse<boolean>) {
+        const collector = res.createMessageComponentCollector({
+            time: CommandInteraction.TIEMPO_ESPERA_INTERACCION
+        });
+
+        collector.on('collect', async button => {
+            if (button.customId === 'buttonCode') {
+                let modal: ModalSubmitInteraction<CacheType>;
+                try {
+                    await button.showModal(this.createModal());
+                    modal = await button.awaitModalSubmit({ time: CommandInteraction.TIEMPO_ESPERA_INTERACCION });
+                    await modal.deferReply({ ephemeral: true });
+                    await this.validateToken(modal);
+                } catch (error) {
+                    console.error(error);
+                    if (modal) {
+                        modal.editReply({
+                            embeds: [
+                                new EmbedBuilder()
+                                    .setColor(Embed.COLOR_RED)
+                                    .setDescription('Ha ocurrido un error. Intentalo de nuevo mas tarde.')
+                            ]
+                        })
+                    }
+                }
             }
+        })
+    }
+
+    private createModal () {
+        const codeInput = new TextInputBuilder()
+            .setCustomId('inputCode')
+            .setLabel('Tu código')
+            .setStyle(TextInputStyle.Paragraph);
+
+        const row = new ActionRowBuilder<TextInputBuilder>().addComponents(codeInput);
+
+        const modal = new ModalBuilder()
+            .setCustomId('modalCode')
+            .setTitle('Pega tu código aquí');
+
+        return modal.addComponents(row);
+    }
+
+    private async validateToken (modal: ModalSubmitInteraction<CacheType>) {
+        const token = modal.fields.getTextInputValue('inputCode');
+
+        const query = `
+            query {
+                Viewer {
+                    id
+                    name
+                    siteUrl
+                }
+            }
+        `;
+
+        const results = await AnilistAPI.authorizedFetch(token, query);
+
+        if (results.errors || !results.data) {
+            await modal.editReply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor(Embed.COLOR_RED)
+                        .setDescription('Token ingresado invalido. Verifica que lo hayas copiado bien.')
+                ]
+            })
+
+            console.error(results.errors);
+            return;
         }
 
+        try {
+            await this.saveUser(results.data.Viewer, token);
+
+            const desc = `
+Autentificación completada correctamente como [${results.data.Viewer.name}](${results.data.Viewer.siteUrl})
+▸ Puedes utilizar \`/usuario\` para comprobar tu perfil.                
+▸ Si lo deseas o cambias de cuenta de anilist, deberás utilizar \`/unsetup\` para desvincular tu cuenta y luego realizar este proceso nuevamente.
+▸ Para conocer otros comandos, utiliza \`/help\`.
+`
+
+            const embedAuthorization = new EmbedBuilder()
+                .setDescription(desc)
+                .setColor(Embed.COLOR_GREEN)
+
+            await modal.editReply ({
+                embeds: [embedAuthorization]
+            })
+        } catch (error) {
+            console.error(error);
+            await modal.editReply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor(Embed.COLOR_RED)
+                        .setDescription('Ha ocurrido al intentar guardar el usuario. Intentalo de nuevo mas tarde.')
+                ]
+            })
+
+            return;
+        }
+    }
+
+    private async saveUser (viewer: Viewer, token: string) {
         await Postgres.query().begin(async sql => {
+            await sql `
+                DELETE FROM 
+                    discord_user
+                WHERE
+                    id_user != ${this.interaction.user.id} and
+                    id_anilist = ${viewer.id};
+            `;
+
             await sql ` 
                 SELECT insert_user (
-                    ${userId},
-                    ${anilistUser.getId()}
+                    ${this.interaction.user.id},
+                    ${viewer.id},
+                    ${token}
                 );
             `;
 
             await sql `
                 SELECT insert_membership (
-                    ${userId},
-                    ${serverId}
+                    ${this.interaction.user.id},
+                    ${this.interaction.guild.id}
                 );
             `;
         });
-
-        const embed = Embed.Crear()
-            .establecerColor(Embed.COLOR_VERDE)
-            .establecerDescripcion('Te has registrado con éxito.')
-            .obtenerDatos();
-
-        try {
-            await this.interaction.reply({
-                embeds: [embed],
-                ephemeral: true
-            })
-        } catch (error) {
-            throw error;
-        }
     }
+}
+
+const EMBED_INSTRUCTIONS_DESCRIPTION = `
+▸ Haz click en el botón **Autorizar**.
+▸ Una vez dentro de la página web, debes presionar en **Authorize**.
+▸ Se generará tu código de autentificación, debes copiarlo.
+▸ Cierra la página web y vuelve aquí, haz click en el botón **Ingresar código**.
+▸ Pega el código en el formulario y envíalo.
+`;
+
+export type Viewer = {
+    id: number,
+    name: string,
+    siteUrl: string
 }
