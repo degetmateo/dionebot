5de1d6e7f18bac091c3d9fbca11770247e94e4b6
Mateo Deget <degetmateo@gmail.com>
2025-08-14 15:17:37 -0300

    removed server

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,8 @@
 import 'dotenv/config';
-import path from 'path';
-import express from 'express';
-import { INVITE_URL, PORT, TOKEN } from "./consts";
+import { TOKEN } from "./consts";
 import Bot from './extensions/bot';
 import postgres from './database/postgres';
 
 const bot = new Bot();
 postgres.init();
 bot.login(TOKEN);
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, '../public')));
-
-app.get('/', (_, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
-app.get('/invite', (_, res) => {
-    res.redirect(INVITE_URL);
-});
-
-app.listen(PORT);
